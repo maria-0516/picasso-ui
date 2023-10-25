759daef20a4ccd4fc963f071bc0fbdca2b115c4c
rabbit0516 <irina880011@gmail.com>
2023-10-25 11:40:25 +0900

    add component - FeartureBox

--- src/components/components/FeatureBox.tsx
+++ src/components/components/FeatureBox.tsx
@@ -20,7 +20,147 @@ const Featurebox = () => {
     const [state, { translateLang }] = useBlockchainContext() as any;
 
     return (
-        <div></div>
+        <div className="row gy-5">
+            <div className="col-lg-6 col-md-6">
+                <div className="feature-box f-boxed style-3">
+                    <Reveal
+                        className="onStep"
+                        keyframes={fadeInUp}
+                        delay={0}
+                        duration={600}
+                        triggerOnce
+                    >
+                        <i className="bg-color-2 i-boxed icon_wallet"></i>
+                    </Reveal>
+                    <div className="text">
+                        <Reveal
+                            className="onStep"
+                            keyframes={fadeInUp}
+                            delay={100}
+                            duration={600}
+                            triggerOnce
+                        >
+                            <h4 className="color">{translateLang('feature_title1')}</h4>
+                        </Reveal>
+                        <Reveal
+                            className="onStep"
+                            keyframes={fadeInUp}
+                            delay={200}
+                            duration={600}
+                            triggerOnce
+                        >
+                            <p className="feature-font">{translateLang('feature_txt1')}</p>
+                        </Reveal>
+                    </div>
+                    <i className="wm icon_wallet"></i>
+                </div>
+            </div>
+
+            <div className="col-lg-6 col-md-6">
+                <div className="feature-box f-boxed style-3">
+                    <Reveal
+                        className="onStep"
+                        keyframes={fadeInUp}
+                        delay={0}
+                        duration={600}
+                        triggerOnce
+                    >
+                        <i className=" bg-color-2 i-boxed icon_pencil-edit_alt"></i>
+                    </Reveal>
+                    <div className="text">
+                        <Reveal
+                            className="onStep"
+                            keyframes={fadeInUp}
+                            delay={100}
+                            duration={600}
+                            triggerOnce
+                        >
+                            <h4 className="color">{translateLang('feature_title2')}</h4>
+                        </Reveal>
+                        <Reveal
+                            className="onStep"
+                            keyframes={fadeInUp}
+                            delay={200}
+                            duration={600}
+                            triggerOnce
+                        >
+                            <p className="feature-font">{translateLang('feature_txt2')}</p>
+                        </Reveal>
+                    </div>
+                    <i className="wm icon_pencil-edit_alt"></i>
+                </div>
+            </div>
+
+            <div className="col-lg-6 col-md-6">
+                <div className="feature-box f-boxed style-3">
+                    <Reveal
+                        className="onStep"
+                        keyframes={fadeInUp}
+                        delay={0}
+                        duration={600}
+                        triggerOnce
+                    >
+                        <i className=" bg-color-2 i-boxed icon_images"></i>
+                    </Reveal>
+                    <div className="text">
+                        <Reveal
+                            className="onStep"
+                            keyframes={fadeInUp}
+                            delay={100}
+                            duration={600}
+                            triggerOnce
+                        >
+                            <h4 className="color">{translateLang('feature_title3')}</h4>
+                        </Reveal>
+                        <Reveal
+                            className="onStep"
+                            keyframes={fadeInUp}
+                            delay={200}
+                            duration={600}
+                            triggerOnce
+                        >
+                            <p className="feature-font">{translateLang('feature_txt3')}</p>
+                        </Reveal>
+                    </div>
+                    <i className="wm icon_images"></i>
+                </div>
+            </div>
+
+            <div className="col-lg-6 col-md-6">
+                <div className="feature-box f-boxed style-3">
+                    <Reveal
+                        className="onStep"
+                        keyframes={fadeInUp}
+                        delay={0}
+                        duration={600}
+                        triggerOnce
+                    >
+                        <i className="bg-color-2 i-boxed icon_tags_alt"></i>
+                    </Reveal>
+                    <div className="text">
+                        <Reveal
+                            className="onStep"
+                            keyframes={fadeInUp}
+                            delay={100}
+                            duration={600}
+                            triggerOnce
+                        >
+                            <h4 className="color">{translateLang('feature_title4')}</h4>
+                        </Reveal>
+                        <Reveal
+                            className="onStep"
+                            keyframes={fadeInUp}
+                            delay={200}
+                            duration={600}
+                            triggerOnce
+                        >
+                            <p className="feature-font">{translateLang('feature_txt4')}</p>
+                        </Reveal>
+                    </div>
+                    <i className="wm icon_tags_alt"></i>
+                </div>
+            </div>
+        </div>
     );
 };
 
